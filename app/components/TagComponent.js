549cd0b7cd8a6e5fce77a439117e1b3aa27eeda5
LuisRizo <personal.luisrizo@gmail.com>
2018-01-18 16:03:45 -0500

    Display a list of all tags and the amount of articles under that tag

--- app/components/TagComponent.js
+++ app/components/TagComponent.js
@@ -1,19 +1,67 @@
 import React, { Component } from 'react'
-import glamorous, { Text } from 'glamorous-native'
+import { Dimensions, Text } from 'react-native'
+import glamorous, { View } from 'glamorous-native'
 
 const TagContainer = glamorous.touchableOpacity({
   padding: 5,
-  margin: 10,
-  backgroundColor: '#999',
-  alignSelf: 'center',
+  margin: 5,
+  backgroundColor: 'white',
+  flexDirection: 'row',
+  borderColor: '#418FBE',
+  borderWidth: 1,
+  justifyContent: 'center',
+  maxWidth: Dimensions.get('window').width / 2 - 15,
   borderRadius: 4,
 })
 
+const TagText = glamorous.text({
+  marginLeft: 7,
+  maxWidth: Dimensions.get('window').width / 3,
+})
+
+const CountContainer = glamorous.view({
+  padding: 3,
+  width: 25,
+  height: 25,
+  backgroundColor: 'grey',
+  borderRadius: 4,
+  marginHorizontal: 5,
+  alignItems: 'center',
+  justifyContent: 'center',
+})
 export default class Tag extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pressed: false,
+    }
+  }
+
+  //TODO: Changing the background color on press works as long as the user does long presses
+  //on short presses, the background color looks black.
+
+  handlePressIn = () => {
+    this.setState({ pressed: true })
+  }
+
+  handlePressOut = () => {
+    this.setState({ pressed: false })
+  }
+
   render() {
+    const { pressed } = this.state
     return (
-      <TagContainer onPress={this.props.onPress}>
-        <Text>{this.props.tag}</Text>
+      <TagContainer
+        // style={{ backgroundColor: pressed ? '#418FB4' : 'transparent' }}
+        // onPressIn={this.handlePressIn}
+        // onPressOut={this.handlePressOut}
+        onPress={this.props.onPress}>
+        <View>
+          <TagText>{this.props.tag}</TagText>
+        </View>
+        <CountContainer>
+          <Text color="white">{this.props.count}</Text>
+        </CountContainer>
       </TagContainer>
     )
   }
--- app/screens/TagsScreen.js
+++ app/screens/TagsScreen.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text, Image } from 'react-native'
+import { View, TouchableOpacity, Text, Image, FlatList } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import TagComponent from '../components/TagComponent'
 import DataService from '../lib/dataInstance'
 import { mixWebsites } from '../lib/functions'
+import glamorous from 'glamorous-native'
+
+const TagsList = glamorous.view({
+  alignItems: 'flex-start',
+  justifyContent: 'center',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+})
 
 export default class TagsScreen extends Component {
   constructor(props) {
@@ -33,7 +41,6 @@ export default class TagsScreen extends Component {
       data.map(item => {
         if (item.hasOwnProperty('tags')) {
           item.tags.split(', ').map(tag => {
-            console.log(tags, tag)
             if (tags.hasOwnProperty(tag)) {
               tags[tag].count += 1
             } else {
@@ -43,15 +50,12 @@ export default class TagsScreen extends Component {
         }
       })
       tagsArray = []
-      console.log(tags)
       for (let item in tags) {
         tagsArray.push({ tag: item, count: tags[item].count })
       }
-      console.log(tagsArray)
       tagsArray.sort((a, b) => {
-        return a.count - b.count
+        return b.count - a.count
       })
-      console.log(tagsArray)
       return tagsArray
     }
     return null
@@ -59,9 +63,26 @@ export default class TagsScreen extends Component {
 
   render() {
     return (
-      <View>
-        <Text>This is the tags screen</Text>
-        <TagComponent tag={'Business'} />
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <FlatList
+            numColumns={2}
+            data={this.getTags()}
+            keyExtractor={(item, index) => index}
+            contentContainerStyle={{
+              alignItems: 'center',
+            }}
+            renderItem={({ item }) => (
+              <TagComponent tag={item.tag} count={item.count} />
+            )}
+          />
+        </View>
+        {/* <TagsList>
+          <TagComponent tag={'Business'} />
+          {this.getTags().map((tag, index) => {
+            return <TagComponent key={index} tag={tag.tag} />
+          })}
+        </TagsList> */}
       </View>
     )
   }
